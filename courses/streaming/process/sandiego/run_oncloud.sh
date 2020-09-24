54c3b4f26e7e16bad244768a179b09c78c197041
Dharmendra Kanjariya <39573806+idjey@users.noreply.github.com>
2020-09-23 22:30:48 -0400

    Update run_oncloud.sh

--- courses/streaming/process/sandiego/run_oncloud.sh
+++ courses/streaming/process/sandiego/run_oncloud.sh
@@ -6,9 +6,9 @@ if [ "$#" -lt 3 ]; then
    exit
 fi
 
-PROJECT=$1
+PROJECT=qwiklabs-gcp-02-60c705975756
 shift
-BUCKET=$1
+BUCKET=qwiklabs-gcp-02-60c705975756
 shift
 MAIN=com.google.cloud.training.dataanalyst.sandiego.$1
 shift
